7f8f81f96f63681ac2f664824a1319f333e4fcf8
nghieee <145263187+nghieee@users.noreply.github.com>
2023-11-17 15:16:08 +0700

    update

---
+++ Areas/Admin/Controllers/SupplierController.cs
@@ -0,0 +1,64 @@
+﻿using DoAn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn.Areas.Admin.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Admin/Supplier
+        public ActionResult FindAll()
+        {
+            mapSupplier map = new mapSupplier();
+            return View(map.FindAll());
+        }
+        //Thêm mới
+        public ActionResult AddNew()
+        {
+            return View(new Supplier());
+        }
+        [HttpPost]
+        public ActionResult AddNew(Supplier model)
+        {
+            var map = new mapSupplier();
+            var id = map.AddNew(model);
+            if (id > 0) return RedirectToAction("FindAll");
+            else
+            {
+                ModelState.AddModelError("", "Lỗi thêm dữ liệu, vui lòng thử lại!");
+                return View(model);
+            }
+        }
+        //Form nhập liệu cập nhật
+        public ActionResult Update(int Supplier_ID)
+        {
+            var order = new mapSupplier().Detail(Supplier_ID);
+            return View(order);
+        }
+        //Hàm lưu
+        [HttpPost]
+        public ActionResult Update(Supplier model)
+        {
+            var map = new mapSupplier();
+            //Hàm cập nhật: được => true, lỗi => false
+            if (map.Update(model) == true)
+            {
+                return RedirectToAction("FindAll");
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+        //Xóa
+        public ActionResult Delete(int Supplier_ID)
+        {
+            var map = new mapSupplier();
+            map.Delete(Supplier_ID);
+            return RedirectToAction("FindAll");
+        }
+    }
+}
---
+++ Models/mapSupplier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAn.Models
+{
+    public class mapSupplier
+    {
+        dbNhanvanEntities db = new dbNhanvanEntities();
+        //Lấy danh sách các supplier đang có
+        public List<Supplier> FindAll()
+        {
+            return db.Suppliers.ToList();
+        }
+        //Thêm mới
+        public int AddNew(Supplier model)
+        {
+            db.Suppliers.Add(model);
+            db.SaveChanges();
+            return model.Supplier_ID;
+        }
+        //Lấy đối tượng theo ID
+        public Supplier Detail(int Supplier_ID)
+        {
+            return db.Suppliers.Find(Supplier_ID);
+        }
+        //Hmaf update
+        public bool Update(Supplier model)
+        {
+            var supplier = db.Suppliers.Find(model.Supplier_ID);
+            supplier.Supplier_ID = model.Supplier_ID;
+            supplier.Supplier_Name = model.Supplier_Name;
+            db.SaveChanges();
+            return true;
+        }
+        //Hàm xóa
+        public void Delete(int Sup_ID)
+        {
+            var supplier = db.Suppliers.Find(Sup_ID);
+            db.Suppliers.Remove(supplier);
+            db.SaveChanges();
+        }
+    }
+}
